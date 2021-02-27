03297437ced96738535d2a359f79e8d358eae60f
Shunsuke135 <shun.135.2109@gmail.com>
2021-02-27 04:28:24 +0000

    fix ランキング機能バグ修正

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -3,6 +3,10 @@ class SearchController < ApplicationController
   def search
 
     @playlist = Playlist.new
+    @playlist_ranking = Playlist.left_outer_joins(:favorites).
+                                group(:id).select('playlists.*, COUNT(favorites.id) AS favorites_count').
+                                order(favorites_count: :desc).page(params[:page])
+
 
     if params[:genre] == "user"
       # if params[:direction] == "keyword"    #部分一致だけなので選択欄不要
@@ -11,13 +15,12 @@ class SearchController < ApplicationController
 
     elsif params[:genre] == "playlist"
       # if  params[:direction] == "keyword"
-          @playlist_ranking = Playlist.where('name LIKE ?', "%#{params[:q]}%").page(params[:page])
+          @playlist_ranking = Playlist.where('name LIKE ?', "%#{params[:q]}%").left_outer_joins(:favorites).
+                              group(:id).select('playlists.*, COUNT(favorites.id) AS favorites_count').
+                              order(favorites_count: :desc).page(params[:page])
     end
       # end
 
-    @playlist_ranking = Playlist.left_outer_joins(:favorites).
-                          group(:id).select('playlists.*, COUNT(favorites.id) AS favorites_count').
-                          order(favorites_count: :desc).page(params[:page])
   end
 
 end
