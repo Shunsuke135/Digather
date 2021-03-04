2c69eb300e80d4f656e101a65905f9be259b5053
Shunsuke135 <shun.135.2109@gmail.com>
2021-03-04 10:08:59 +0000

    add ゲストログイン機能追加

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -1,10 +1,20 @@
 class HomesController < ApplicationController
-  before_action :authenticate_user!, except: [:top, :about]
+ before_action :authenticate_user!, except: [:top, :about, :new_guest]
 
   def top
   end
 
   def about
+  end
+
+  def new_guest
+    user = User.find_or_create_by!(email: 'guest@example.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.name = 'ゲスト'
+      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
+    end
+    sign_in user
+    redirect_to user_path(current_user), notice: 'Signed in successfully.'
   end
 
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  post '/homes/guest_sign_in', to: 'homes#new_guest'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   resources :users, only: [:show,:index,:edit,:update] do
     resource :relationships, only: [:create, :destroy]
