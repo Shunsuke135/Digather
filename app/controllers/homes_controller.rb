b043da0dc49f6229dab3bfbf976a9e9ffc945c40
Shunsuke135 <shun.135.2109@gmail.com>
2021-03-25 14:34:14 +0000

    clean: 一部リファクタリング

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -11,7 +11,6 @@ class HomesController < ApplicationController
     user = User.find_or_create_by!(email: 'guest@example.com') do |user|
       user.password = SecureRandom.urlsafe_base64
       user.name = 'ゲスト'
-      # user.confirmed_at = Time.now  # Confirmable を使用している場合は必要
     end
     sign_in user
     redirect_to user_path(current_user), notice: 'Signed in successfully.'  #サインイン後はuser/show画面に遷移
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -4,12 +4,12 @@ class RelationshipsController < ApplicationController
 
   def create
     current_user.follow(@user)
-    # redirect_back fallback_location: root_url
+    # redirect_back fallback_location: root_url #非同期通信実装の為にコメントアウト
   end
 
   def destroy
     current_user.unfollow(@user)
-    # redirect_back fallback_location: root_url
+    # redirect_back fallback_location: root_url #非同期通信実装の為にコメントアウト
   end
 
   def followings
